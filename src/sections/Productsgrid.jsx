36218df014c9bbff86a9fb364716b9af4122c644
Bingwa001 <onebingwa7@gmail.com>
2025-06-14 17:06:55 -0700

    Initial commit: Electronic ecommerce project setup

---
+++ src/sections/Productsgrid.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Productsgrid = () => {
+  return (
+    <div>Productsgrid</div>
+  );
+}
+
+export default Productsgrid;
+
---
+++ src/sections/Types.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Types = () => {
+  return (
+    <div>Types</div>
+  );
+}
+
+export default Types;
+
+
+
